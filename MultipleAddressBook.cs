60aad65954577014b6aac2ec70168029ed86cf21
entropy-sumit <sumitr600@gmail.com>
2022-01-09 20:31:12 +0530

    [Add] sumit added uc6 program

---
+++ MultipleAddressBook.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class MultipleAddressBook
+    {
+        public static List<PersonsDetails> contacts = new List<PersonsDetails>();
+
+        public static Dictionary<string, List<PersonsDetails>> addressBook1 = new Dictionary<string, List<PersonsDetails>>();
+
+        public static void AddTo(string name)
+        {
+            addressBook1.Add(name, contacts);
+        }
+        public static void AddContact()
+        {
+            PersonsDetails person = new PersonsDetails();
+
+            Console.Write(" Enter your First name : ");
+            person.Firstname = Console.ReadLine();
+            Console.Write(" Enter your Last name : ");
+            person.LastName = Console.ReadLine();
+            Console.Write(" Enter your Address : ");
+            person.Address = Console.ReadLine();
+            Console.Write(" Enter your City : ");
+            person.City = Console.ReadLine();
+            Console.Write(" Enter your State : ");
+            person.State = Console.ReadLine();
+            Console.Write(" Enter your Zip Code : ");
+            person.ZipCode = Console.ReadLine();
+            Console.Write(" Enter your Phone Number : ");
+            person.PhoneNumber = Console.ReadLine();
+            Console.Write(" Enter your Email-ID : ");
+            person.Email = Console.ReadLine();
+
+            contacts.Add(person);
+            Console.WriteLine("\n {0}'s contact succesfully added", person.Firstname);
+        }
+
+        public static void Details()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("Currently there are no people added in your address book.");
+            }
+            else
+            {
+                Console.WriteLine("Here is the list and details of all the contacts in your addressbook.");
+
+                foreach (var Detailing in contacts)
+                {
+                    Console.WriteLine("First name : " + Detailing.Firstname);
+                    Console.WriteLine("Last name : " + Detailing.LastName);
+                    Console.WriteLine("Address : " + Detailing.Address);
+                    Console.WriteLine("State : " + Detailing.State);
+                    Console.WriteLine("City : " + Detailing.City);
+                    Console.WriteLine("Zip Code : " + Detailing.ZipCode);
+                    Console.WriteLine("Phone number = " + Detailing.PhoneNumber);
+                }
+            }
+        }
+    }
+}
